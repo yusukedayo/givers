2426f846ff8c5f23ee483e7809643ceed156645b
yusukedayo <tsunemi0217@icloud.com>
2022-04-25 16:05:29 +0900

    Fix: Bootstrapのcssが反映されないバグを改善 fontawsomeを導入するために追加したコードが原因でcssが気かなくなっていたので一旦削除した。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import jquery from "jquery"
-window.$ = window.jQuery = jquery
-import * as bootstrap from "bootstrap"
-window.bootstrap = bootstrap
+import "bootstrap";
+import "bootstrap"
 import "../stylesheets/application"
 
 import '@fortawesome/fontawesome-free/js/all';
