5098ab6c72544c29a43c4bb79a29bfb9d5811736
yusukedayo <tsunemi0217@icloud.com>
2022-04-27 00:04:33 +0900

    Add: likeモデルの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -22,7 +22,8 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-
+  has_many :likes, dependent: :destroy
+  has_many :like_users, through: :likes, source: :user
   validates :url, :status, presence: true
   enum status: { everytime: 0, after_study: 1, before_study: 2 }
 end
