98ad0137fd48f3aca990b203bd35f650f80cae00
yusukedayo <tsunemi0217@icloud.com>
2022-04-28 15:12:17 +0900

    bootstrapファイルを編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '@fortawesome/fontawesome-free/js/all';
+import jquery from "jquery"
+window.$ = window.jQuery = jquery
+import * as bootstrap from "bootstrap"
+window.bootstrap = bootstrap
 import "../stylesheets/application";
 
 
