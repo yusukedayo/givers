51fbb19ec1b80a6adff0bf5917b8f0164cc2b1f2
yusukedayo <tsunemi0217@icloud.com>
2022-05-09 22:24:25 +0900

    Add: 画像をランダム表示するために画像を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def active?(controller_name, action_name)
      return "active border-bottom border-dark border-2" if controller_name == params[:controller] && action_name == params[:action]
   end
+
+  def rundum_image
+    images = ['underrunte.jpeg', 'niserunte.png', 'run.png']
+    article_image = images.sort_by{rand}.first
+  end
 end
