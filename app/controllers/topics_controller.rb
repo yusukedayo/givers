8e2bc7b3138aa2d48ca1894feb5339a1bca3fa5a
yusukedayo <tsunemi0217@icloud.com>
2022-05-09 21:34:22 +0900

    Add: kaminari実装のためにコントローラーを変更

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,50 +1,50 @@
 class TopicsController < ApplicationController
   def index
-    @articles = Article.all.includes(:user).order(created_at: :desc)
+    @articles = Article.all.includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def introduction
-     @introduction_articles = Article.tagged_with(["入門"], :wild => true).includes(:user).order(created_at: :desc)
+     @introduction_articles = Article.tagged_with(["入門"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def basis
-    @basis_articles = Article.tagged_with(["基礎"], :wild => true).includes(:user).order(created_at: :desc)
+    @basis_articles = Article.tagged_with(["基礎"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def advance
-    @advance_articles = Article.tagged_with(["応用"], :wild => true).includes(:user).order(created_at: :desc)
+    @advance_articles = Article.tagged_with(["応用"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def advance
-    @advance_articles = Article.tagged_with(["応用"], :wild => true).includes(:user).order(created_at: :desc)
+    @advance_articles = Article.tagged_with(["応用"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def error
-    @error_articles = Article.tagged_with(["エラー"], :wild => true).includes(:user).order(created_at: :desc)
+    @error_articles = Article.tagged_with(["エラー"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def function
-    @function_articles = Article.tagged_with(["機能"], :wild => true).includes(:user).order(created_at: :desc)
+    @function_articles = Article.tagged_with(["機能"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def design
-    @design_articles = Article.tagged_with(["デザイン"], :wild => true).includes(:user).order(created_at: :desc)
+    @design_articles = Article.tagged_with(["デザイン"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def rails
-    @rails_articles = Article.tagged_with(["ails"], :wild => true).includes(:user).order(created_at: :desc)
+    @rails_articles = Article.tagged_with(["ails"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def how_to_study
-    @how_to_study_articles = Article.tagged_with(["学習"], :wild => true).includes(:user).order(created_at: :desc)
+    @how_to_study_articles = Article.tagged_with(["学習"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def life_hack
-    @life_hack_articles = Article.tagged_with(["ライフハック"], :wild => true).includes(:user).order(created_at: :desc)
+    @life_hack_articles = Article.tagged_with(["ライフハック"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def event
-    @event_articles = Article.tagged_with(["イベント"], :wild => true).includes(:user).order(created_at: :desc)
+    @event_articles = Article.tagged_with(["イベント"], :wild => true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
 end
