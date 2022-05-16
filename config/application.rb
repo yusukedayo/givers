b81faa6f621c7d1a4acc450a71b26acf1258af31
yusukedayo <tsunemi0217@icloud.com>
2022-05-16 17:05:17 +0900

    Change: 時刻を日本語に変更

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module Givers
     config.assets.initialize_on_precompile = false
     config.i18n.available_locales = %i[ja en]
     config.i18n.default_locale = :ja
-  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
 
     # Configuration for the application, engines, and railties goes here.
     #
